a0de90ee216bead2626da6d41907efa4b8352ba0
buefox <bue830727@gmail.com>
2016-04-19 21:33:38 +0800

    done

--- view_counting/ViewController3.swift
+++ view_counting/ViewController3.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController3.swift
 //  view_counting
 //
 //  Created by ANDYYU on 2016/4/19.
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController2: UIViewController {
+    
     @IBOutlet weak var Counter: UILabel!
     var count:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view,
+        // Do any additional setup after loading the view, typically from a nib.
         //self.count = 0
     }
     override func viewDidAppear(animated: Bool) {
@@ -24,13 +25,15 @@ class ViewController: UIViewController {
         else{
             self.Counter.text = "You visited \(count) times.\n"
         }
-            
+    }
+    override func viewDidDisappear(animated: Bool) {
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
